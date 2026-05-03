11014f11733f29fa2a17f6acec0337c7d638b015
Priyanka Sambhaji Tribhuvan <priyankatribhuvan652@gmail.com>
2026-05-03 13:28:34 +0530

    Delete supabase.js

--- supabase.js
+++
@@ -1,4 +0,0 @@
-const supabaseClient = window.supabase.createClient(
-  "https://objwfboldhhxygbqbfcx.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9iandmYm9sZGhoeHlnYnFiZmN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg4ODM1NDIsImV4cCI6MjA4NDQ1OTU0Mn0.gWi5hGk-jNdm3yJHHqunvUMLHjFA_aOAjbPMobRQoXM"
-);
